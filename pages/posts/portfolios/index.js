c64f43840f431999108cb64add3a74b9208552bd
MartinPettersen <searchingtheskyfor@gmail.com>
2022-05-19 17:25:40 +0200

    getting info from github

--- pages/posts/portfolios/index.js
+++ pages/posts/portfolios/index.js
@@ -1,8 +1,12 @@
 import Axios from 'axios';
 import Link from "next/link";
 import Card from '../../../components/card';
-const PortfolioList = ({repositories}) => {
+const PortfolioList = ({repositories, repList}) => {
   console.log(repositories[0].name);
+  console.log('<<<<<<<<<<<<<<<')
+  console.log(repList);
+  console.log(repList[0]);
+  console.log('>>>>>>>>>>>>>>>>>>')
   return (
     <div>
       <h1>Portfolio Page</h1>
@@ -20,12 +24,30 @@ const PortfolioList = ({repositories}) => {
   )
 }
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   const data = await Axios.get("https://api.github.com/users/MartinPettersen/repos?")
 
+  const repositorieList = [];
+  data.data.map(async (repositorie) => {
+    const languages = await Axios.get(repositorie.languages_url);
+    const readme = await Axios.get(repositorie.url + "/contents/README.md"); 
+
+    // console.log('link: '+ repositorie.url + "/contents/README.md" )
+    // console.log(languages.data)
+    // console.log(repositorie.languages_url)
+    repositorieList.push({
+      name: repositorie.name,
+      htmlUrl: repositorie.html_url,
+      languages,
+      readme,
+    })
+  }
+  );
+  console.log(repositorieList);
   return {
     props: {
-      repositories: data.data
+      repositories: data.data,
+      repList: repositorieList
     }
   }
 }
