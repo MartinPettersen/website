f780196a118caac392ab9a628ed508a91e5615ce
MartinPettersen <searchingtheskyfor@gmail.com>
2022-06-01 14:39:35 +0200

    removed content from index

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,14 @@ import styles from '../styles/Home.module.css'
 import Sidebar from '../components/sidebar'
 export default function Home() {
   return (
-    <div className='index_container'>
+    <>
 
-    <div className={styles.container}>
       <Head>
         <title>Portfolio Site</title>
         <meta name="description" content="Portfolio Page" />
       </Head>
 
-    <p>Hei der</p>
 
-    </div>
-    </div>
+    </>
   )
 }
